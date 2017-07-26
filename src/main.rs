294f5061b231e72258f2ff30735753315ed80a91
Matt Yaraskavitch <yaraskavitch.matt@gmail.com>
2017-07-26 01:14:33 -0400

    Added serde serialization

--- src/main.rs
+++ src/main.rs
@@ -1,38 +1,50 @@
 extern crate futures;
 extern crate hyper;
 extern crate tokio_core;
+extern crate serde_json;
+
+#[macro_use]
+extern crate serde_derive;
+
+use serde_json::Error;
 
 use std::io::{self, Write};
 use std::collections::BTreeMap;
 use futures::{Future, Stream};
 use hyper::Client;
 use tokio_core::reactor::Core;
+use serde_json::Value;
 
 const APP_ID: &str = "56b8dbfc7d0e4425b9d42c53a274e3e2";
 const URL_LATEST: &str = "http://openexchangerates.org/api/latest.json";
 
-struct Lastest_ex<'a> {
+#[derive(Serialize, Deserialize, Debug)]
+struct Xchg<'a> {
     disclaimer: &'a str,
     license: &'a str,
     timestamp: usize,
     base: &'a str,
-    rates: BTreeMap<&'a str, f64>
+    rates: BTreeMap<&'a str, f64>,
 }
 
 fn main() {
     let mut core = Core::new().unwrap();
     let client = Client::new(&core.handle());
 
-    let uri = (URL_LATEST.to_owned()+"?app_id="+APP_ID).parse().unwrap();
+    let uri = (URL_LATEST.to_owned() + "?app_id=" + APP_ID)
+        .parse()
+        .unwrap();
     let work = client.get(uri).and_then(|res| {
         println!("Response: {}", res.status());
 
-        res.body().for_each(|chunk| {
-            io::stdout()
-                .write_all(&chunk)
-                .map(|_| ())
-                .map_err(From::from)
+        res.body().concat2().and_then(|body| {
+            let v: Xchg = serde_json::from_slice(&body).map_err(|e| {
+                io::Error::new(io::ErrorKind::Other, e)
+            })?;
+            Ok(v)
         })
     });
-    core.run(work).unwrap();
+
+    let res = core.run(work).unwrap();
+    println!("{:?}", res);
 }
