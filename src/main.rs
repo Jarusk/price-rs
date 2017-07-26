18f6105d297fb3f9b23d4cdb25042abce8d4a378
Matt Yaraskavitch <yaraskavitch.matt@gmail.com>
2017-07-26 01:29:41 -0400

    Removed uneeded imports and changed strings to owned

--- src/main.rs
+++ src/main.rs
@@ -6,26 +6,26 @@ extern crate serde_json;
 #[macro_use]
 extern crate serde_derive;
 
-use serde_json::Error;
 
-use std::io::{self, Write};
+use std::io;
 use std::collections::BTreeMap;
 use futures::{Future, Stream};
 use hyper::Client;
 use tokio_core::reactor::Core;
-use serde_json::Value;
 
 const APP_ID: &str = "56b8dbfc7d0e4425b9d42c53a274e3e2";
 const URL_LATEST: &str = "http://openexchangerates.org/api/latest.json";
 
+
 #[derive(Serialize, Deserialize, Debug)]
-struct Xchg<'a> {
-    disclaimer: &'a str,
-    license: &'a str,
+struct Xchg {
+    disclaimer: String,
+    license: String,
     timestamp: usize,
-    base: &'a str,
-    rates: BTreeMap<&'a str, f64>,
+    base: String,
+    rates: BTreeMap<String, f64>,
 }
+
 
 fn main() {
     let mut core = Core::new().unwrap();
@@ -34,17 +34,16 @@ fn main() {
     let uri = (URL_LATEST.to_owned() + "?app_id=" + APP_ID)
         .parse()
         .unwrap();
-    let work = client.get(uri).and_then(|res| {
-        println!("Response: {}", res.status());
 
+    let work = client.get(uri).and_then(|res| {
         res.body().concat2().and_then(|body| {
-            let v: Xchg = serde_json::from_slice(&body).map_err(|e| {
-                io::Error::new(io::ErrorKind::Other, e)
-            })?;
+            let v: Xchg = serde_json::from_slice(&body)
+                .map_err(|e| io::Error::new(io::ErrorKind::Other, e))
+                .unwrap();
             Ok(v)
         })
     });
 
     let res = core.run(work).unwrap();
-    println!("{:?}", res);
+    println!("{:?}", &res);
 }
