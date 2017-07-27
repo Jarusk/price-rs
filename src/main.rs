844e21e082eb7d904716120bc45143507561ddc8
Matt Yaraskavitch <yaraskavitch.matt@gmail.com>
2017-07-27 01:05:44 -0400

    Added more work towards config file loading

--- src/main.rs
+++ src/main.rs
@@ -25,25 +25,17 @@ extern crate time;
 #[macro_use]
 extern crate serde_derive;
 
+mod xchg;
+mod config;
+mod constants;
 
 use std::io;
-use std::collections::BTreeMap;
 use futures::{Future, Stream};
 use hyper::Client;
 use tokio_core::reactor::Core;
 
 const APP_ID: &str = "56b8dbfc7d0e4425b9d42c53a274e3e2";
 const URL_LATEST: &str = "http://openexchangerates.org/api/latest.json";
-
-
-#[derive(Serialize, Deserialize, Debug)]
-struct Xchg {
-    disclaimer: String,
-    license: String,
-    timestamp: i64,
-    base: String,
-    rates: BTreeMap<String, f64>,
-}
 
 
 fn main() {
@@ -56,7 +48,7 @@ fn main() {
 
     let work = client.get(uri).and_then(|res| {
         res.body().concat2().and_then(|body| {
-            let v: Xchg = serde_json::from_slice(&body)
+            let v: xchg::Xchg = serde_json::from_slice(&body)
                 .map_err(|e| io::Error::new(io::ErrorKind::Other, e))
                 .unwrap();
             Ok(v)
@@ -65,15 +57,5 @@ fn main() {
 
     let res = core.run(work).unwrap();
 
-    println!("{:?}", xchg_still_valid(&res));
-}
-
-fn xchg_still_valid(stored: &Xchg) -> bool {
-    let now = time::get_time().sec;
-
-    if now - stored.timestamp < 3600 {
-        true
-    } else {
-        false
-    }
+    println!("{:?}", res.still_valid());
 }
