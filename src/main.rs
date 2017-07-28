5599ed28b40ec787b4d9b530f6652ea427c5071d
Matt Yaraskavitch <yaraskavitch.matt@gmail.com>
2017-07-28 02:34:56 -0400

    Moved rate fetching into the Xchg file

--- src/main.rs
+++ src/main.rs
@@ -16,12 +16,6 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-extern crate futures;
-extern crate hyper;
-extern crate tokio_core;
-extern crate serde_json;
-extern crate time;
-
 #[macro_use]
 extern crate serde_derive;
 
@@ -29,33 +23,12 @@ mod xchg;
 mod config;
 mod constants;
 
-use std::io;
-use futures::{Future, Stream};
-use hyper::Client;
-use tokio_core::reactor::Core;
-
-const APP_ID: &str = "56b8dbfc7d0e4425b9d42c53a274e3e2";
-const URL_LATEST: &str = "http://openexchangerates.org/api/latest.json";
-
+use xchg::Xchg;
+use config::Config;
 
 fn main() {
-    let mut core = Core::new().unwrap();
-    let client = Client::new(&core.handle());
+    let conf = Config::load_or_create();
+    let res = Xchg::load_or_create(&conf);
 
-    let uri = (URL_LATEST.to_owned() + "?app_id=" + APP_ID)
-        .parse()
-        .unwrap();
-
-    let work = client.get(uri).and_then(|res| {
-        res.body().concat2().and_then(|body| {
-            let v: xchg::Xchg = serde_json::from_slice(&body)
-                .map_err(|e| io::Error::new(io::ErrorKind::Other, e))
-                .unwrap();
-            Ok(v)
-        })
-    });
-
-    let res = core.run(work).unwrap();
-
-    println!("{:?}", res.still_valid());
+    println!("{:?}", res);
 }
--- src/xchg.rs
+++ src/xchg.rs
@@ -15,10 +15,21 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
-
+extern crate futures;
+extern crate hyper;
+extern crate tokio_core;
 extern crate time;
+extern crate serde_json;
 
+use self::futures::{Future, Stream};
+use self::hyper::Client;
+use self::tokio_core::reactor::Core;
 use std::collections::BTreeMap;
+use std::io;
+use constants;
+use config::Config;
+
+//const APP_ID: &str = "56b8dbfc7d0e4425b9d42c53a274e3e2";
 
 #[derive(Serialize, Deserialize, Debug)]
 pub struct Xchg {
@@ -33,5 +44,33 @@ impl Xchg {
     pub fn still_valid(&self) -> bool {
         let now = time::get_time().sec;
         return now - self.timestamp < 3600;
+    }
+
+    pub fn load_or_create(conf: &Config) -> Xchg {
+        fetch_rates(conf)
+    }
+}
+
+fn fetch_rates(conf: &Config) -> Xchg {
+    let mut core = Core::new().unwrap();
+    let client = Client::new(&core.handle());
+
+    let uri = (constants::URL_LATEST.to_owned() + "?app_id=" + conf.get_app_id())
+        .parse()
+        .unwrap();
+
+    let work = client.get(uri).and_then(|res| {
+        res.body().concat2().and_then(|body| {
+            let v: Xchg = serde_json::from_slice(&body)
+                .map_err(|e| io::Error::new(io::ErrorKind::Other, e))
+                .unwrap();
+
+            Ok(v)
+        })
+    });
+
+    match core.run(work) {
+        Err(e) => panic!("Could not fetch Rates file: {}", e),
+        Ok(x) => x,
     }
 }
